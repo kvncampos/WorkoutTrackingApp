1768ba77055d51672325010e3b4abf37f725012d
kvncampos <kvnelpdfw@hotmail.com>
2023-12-01 01:06:20 -0600

    Modified env variable for Nutrionix

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import datetime
 from Nutritionix.Nutritionix import NutritionixSetup
 import os
-from Nutritionix.creds import NUTRITIONIX_ID, NUTRITIONIX_API_KEY, USER_INFO
+from Nutritionix.creds import USER_INFO
 from Sheety.sheety_api import SheetyApi
 
 
 # ------------------------------------------------------------------------------------------------------------
-# os.environ['nutritionix_id'] = str(input("What is the Nutrionix ID? "))
-# os.environ['nutritionix_api_key'] = str(input("What is the Nutrionix API Key? "))
-#
-# NUTRITIONIX_ID = os.environ['nutritionix_id']
-# NUTRITIONIX_API_KEY = os.environ['nutritionix_api_key']
+os.environ['nutritionix_id'] = str(input("What is the Nutrionix ID? "))
+os.environ['nutritionix_api_key'] = str(input("What is the Nutrionix API Key? "))
+
+NUTRITIONIX_ID = os.environ['nutritionix_id']
+NUTRITIONIX_API_KEY = os.environ['nutritionix_api_key']
 
 # ------------------------------ GET EXERCISE INFORMATION ---------------------------------------------------
 exercise_call = NutritionixSetup(id=NUTRITIONIX_ID, api_key=NUTRITIONIX_API_KEY, user_info=USER_INFO)
 
 new_data = input("What Exercise did you do? Ex: Heavy Weights 60 min\n")
-# workout = exercise_call.post_nl_exercise('heavy weights 60 min')
 workout = exercise_call.post_nl_exercise(new_data)
 exercise = workout['name']
 calories = workout['calories_burned']
